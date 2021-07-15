79b234e7a50447d0da80bb45eb343887afc32d77
Anslate <52884856+Colcactus-ColdVoid@users.noreply.github.com>
2021-07-15 18:49:17 +0800

    Create 冒泡排序.c

---
+++ base/冒泡排序/C/冒泡排序.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+void main(){
+	int lenth,i,temp;
+	int n=1;
+	scanf("%d",&lenth);
+	int a[lenth];
+	for(i=0;i<lenth;i++){
+		scanf("%d",&a[i]);
+	} 
+	while(lenth>n-1){
+		for(i=0;i<lenth-n;i++){
+			if(a[i]<a[i+1]){//<号是从大到小，>号从小到大 
+				temp=a[i];
+				a[i]=a[i+1];
+				a[i+1]=temp;
+			}
+		}
+		n++;
+	}
+	for(i=0;i<lenth;i++){
+		printf("%d ",a[i]);
+	}
+} 
