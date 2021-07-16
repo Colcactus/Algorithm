24f7049a2d2a1e04f655ed60bae49216290248c4
ColdVoid <52884856+Colcactus-ColdVoid@users.noreply.github.com>
2021-07-17 00:20:07 +0800

    Create bubble_sort.cpp

---
+++ base/sort/bubble_sort/cpp/bubble_sort.cpp
@@ -0,0 +1,41 @@
+/*
+输入样式：
+例：
+5
+1 3 2 5 4
+共两行
+第一行n表示第二行的数据数量
+第二行数据是待排序数组，共n个，数据之间加空格
+输出样式：
+例：
+5 4 3 2 1
+共一行
+表示排序完毕的数组
+
+数据不超过int范围
+*/
+#include <cstdio>
+using namespace std;
+int main(){
+	int lenth,i,temp;
+	int n=1;
+	scanf("%d",&lenth);
+	int a[lenth];
+	for(i=0;i<lenth;i++){
+		scanf("%d",&a[i]);
+	} 
+	while(lenth>n-1){
+		for(i=0;i<lenth-n;i++){
+			if(a[i]<a[i+1]){//<号是从大到小，>号从小到大
+				temp=a[i];
+				a[i]=a[i+1];
+				a[i+1]=temp;
+			}
+		}
+		n++;
+	}
+	for(i=0;i<lenth;i++){
+		printf("%d ",a[i]);
+	}
+	return 0;
+} 
