2507ab1700247a0362d428af52a6f723cbe75ea4
ColdVoid <52884856+Colcactus-ColdVoid@users.noreply.github.com>
2021-07-22 03:09:18 +0800

    先写一些

---
+++ base/sort/merge_sort/c/merge_sort.c
@@ -0,0 +1,12 @@
+/*
+这个是我魔改的归并排序，更容易理解
+原归并排序自己查看Wiki
+https://zh.wikipedia.org/wiki/%E5%BD%92%E5%B9%B6%E6%8E%92%E5%BA%8F
+不是迭代，不是递归，就是普通的程序 
+*/
+#include<stdio.h>
+int lenth=7;
+int a[lenth]={0,1,2,3,4,5,6};
+void main(){
+		
+}
