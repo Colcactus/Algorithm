33ed8d977800084b7c17043490d5812496f5cc52
hattori-emi <a7b8i06c49@gmail.com>
2021-07-25 17:17:04 +0800

    更改结构，补充注释

--- base/random_number/julia/linear_congruential_generator.jl
+++ base/random_number/julia/linear_congruential_generator.jl
@@ -22,14 +22,12 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 
 这是一种简单且常见的伪随机数生成算法
-公式如下
+LaTeX公式如下
 
-$$
 X_{n+1} = (A * X_n + B) Mod M
-$$
 
-其中A、B和M是常数，因此在下面的代码中A、B和M是可以取任意其它值的
-输出如下:
+其中A、B和M是常数，因此在下面的代码中A、B和M是可以取任意其它值的，但最优选是A = 25214903917、B = 11、C = 2^48
+原算法输出如下:(A = 7, B = 5, M = 11)
 
 ``` julia
 julia> linear_congruential_generator(2)
@@ -46,15 +44,31 @@ julia> linear_congruential_generator(2)
 8
 ```
 
-根据输出内容我们可以看出循环第11次的时候这组数据开始重复了
+后算法输出如下:
+``` julia
+julia> linear_congruential_generator(2)
+50429807845
+-151174310397044
+197748051927719
+161694279895846
+63052479226681
+-102186399074992
+148544920408859
+113574016328842
+-60143030400307
+167765679818580
+249775507363535
+```
+
+根据原算法输出内容我们可以看出循环第11次的时候这组数据开始重复了
 一般伪随机数生成器使用有多个常数的多元线性同余发生器，并从生成的数据中选择一些数据来使结果看起来更加随机
 顺带一提，Julia的随机数生成函数非常强大，有兴趣的伙伴可以仔细阅读Julia的官方文档哦
 """
 
 function linear_congruential_generator(seed::Int64)
-    A = 7
-    B = 5
-    M = 11
+    A = 25214903917
+    B = 11
+    M = 1 << 48
     X = seed
 
     for i = 1:11
